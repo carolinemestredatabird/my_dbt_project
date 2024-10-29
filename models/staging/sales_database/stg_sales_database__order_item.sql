07c5a24390de7a0412e0c471e3941d6022157e4b
carolinemestre744@gmail.com <carolinemestre744@gmail.com>
2024-10-29 14:13:03 +0000

    fix : Remove column

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -5,6 +5,5 @@ select CONCAT(order_id, '_', product_id) AS order_item_id_x,
  DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
  price as unit_price_x,
  shipping_cost_x,
- quantity as item_quantity_x,
  (price * quantity) + shipping_cost as total_order_item_amount_x
 from {{ source('sales_database', 'order_item') }}
