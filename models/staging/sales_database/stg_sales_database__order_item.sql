47ab7521c890a59b0e93d26c66ffa778ee6e7f37
carolinemestre744@gmail.com <carolinemestre744@gmail.com>
2024-10-29 14:54:09 +0000

    fix error

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,7 +1,7 @@
 select CONCAT(order_id, '_', product_id) AS order_item_id,
  order_id AS order_id,
  product_id AS product_id,
- seller_id AS seller_id
+ seller_id AS seller_id,
  DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
  price as unit_price,
  shipping_cost,
